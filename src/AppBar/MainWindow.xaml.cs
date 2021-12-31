5804de43c71806ba2c86c5c80741cafe3be105e1
Dejan Stojanovic <d.stojanovic@hotmail.com>
2021-12-31 15:07:38 +0400

    feat: initial commit

---
+++ src/AppBar/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WpfAppBar;
+
+namespace AppBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            AppBarFunctions.SetAppBar(this, ABEdge.Top);
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/WinAppBar/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppBar
+{
+    public partial class Main : Form
+    {
+        //private Container components = null;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.BackColor = GetTaskbarColor();
+            RegisterBar();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RegisterBar();
+        }
+
+    }
+}
