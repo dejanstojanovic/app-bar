ed12fd0a579494bfe188fe25d8f33235d8a9efe6
Dejan Stojanovic <d.stojanovic@hotmail.com>
2021-12-31 15:40:21 +0400

    feat: adds context menu

--- src/WinAppBar/Main.Designer.cs
+++ src/WinAppBar/Main.Designer.cs
@@ -33,8 +33,34 @@ namespace WinAppBar
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.contextMenuMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuMain.SuspendLayout();
             this.SuspendLayout();
             // 
+            // panelMain
+            // 
+            this.panelMain.ContextMenuStrip = this.contextMenuMain;
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(800, 39);
+            this.panelMain.TabIndex = 0;
+            // 
+            // contextMenuMain
+            // 
+            this.contextMenuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuMain.Name = "contextMenuMain";
+            this.contextMenuMain.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // Main
             // 
@@ -43,254 +69,22 @@ namespace WinAppBar
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 39);
             this.ControlBox = false;
+            this.Controls.Add(this.panelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Main";
             this.ShowInTaskbar = false;
             this.Text = "Main";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Main_Load);
-            this.FormClosing += Main_FormClosing;
+            this.contextMenuMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-
-        #region Application bar methods
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RECT
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct APPBARDATA
-        {
-            public int cbSize;
-            public IntPtr hWnd;
-            public int uCallbackMessage;
-            public int uEdge;
-            public RECT rc;
-            public IntPtr lParam;
-        }
-
-        private enum ABMsg : int
-        {
-            ABM_NEW = 0,
-            ABM_REMOVE = 1,
-            ABM_QUERYPOS = 2,
-            ABM_SETPOS = 3,
-            ABM_GETSTATE = 4,
-            ABM_GETTASKBARPOS = 5,
-            ABM_ACTIVATE = 6,
-            ABM_GETAUTOHIDEBAR = 7,
-            ABM_SETAUTOHIDEBAR = 8,
-            ABM_WINDOWPOSCHANGED = 9,
-            ABM_SETSTATE = 10
-        }
-
-        private enum ABNotify : int
-        {
-            ABN_STATECHANGE = 0,
-            ABN_POSCHANGED,
-            ABN_FULLSCREENAPP,
-            ABN_WINDOWARRANGE
-        }
-
-        private enum ABEdge : int
-        {
-            ABE_LEFT = 0,
-            ABE_TOP,
-            ABE_RIGHT,
-            ABE_BOTTOM
-        }
-
-        private bool fBarRegistered = false;
-
-        [DllImport("Shell32.dll", CallingConvention = CallingConvention.StdCall)]
-        private static extern uint SHAppBarMessage(int dwMessage, ref APPBARDATA pData);
-
-        [DllImport("User32.dll")]
-        private static extern int GetSystemMetrics(int Index);
-
-        [DllImport("User32.dll", ExactSpelling = true,
-            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
-        private static extern bool MoveWindow
-            (IntPtr hWnd, int x, int y, int cx, int cy, bool repaint);
-
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        private static extern int RegisterWindowMessage(string msg);
-
-        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
-        private static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
-
-
-        private int uCallBack;
-
-        protected void RegisterBar()
-        {
-            APPBARDATA abd = new APPBARDATA();
-            abd.cbSize = Marshal.SizeOf(abd);
-            abd.hWnd = this.Handle;
-            if (!fBarRegistered)
-            {
-                uCallBack = RegisterWindowMessage("AppBarMessage");
-                abd.uCallbackMessage = uCallBack;
-
-                uint ret = SHAppBarMessage((int)ABMsg.ABM_NEW, ref abd);
-                fBarRegistered = true;
-
-                ABSetPos();
-            }
-            else
-            {
-                SHAppBarMessage((int)ABMsg.ABM_REMOVE, ref abd);
-                fBarRegistered = false;
-            }
-        }
-
-        private void ABSetPos()
-        {
-            APPBARDATA abd = new APPBARDATA();
-            abd.cbSize = Marshal.SizeOf(abd);
-            abd.hWnd = this.Handle;
-            abd.uEdge = (int)ABEdge.ABE_TOP;
-
-            if (abd.uEdge == (int)ABEdge.ABE_LEFT || abd.uEdge == (int)ABEdge.ABE_RIGHT)
-            {
-                abd.rc.top = 0;
-                abd.rc.bottom = SystemInformation.PrimaryMonitorSize.Height;
-                if (abd.uEdge == (int)ABEdge.ABE_LEFT)
-                {
-                    abd.rc.left = 0;
-                    abd.rc.right = Size.Width;
-                }
-                else
-                {
-                    abd.rc.right = SystemInformation.PrimaryMonitorSize.Width;
-                    abd.rc.left = abd.rc.right - Size.Width;
-                }
-            }
-            else
-            {
-                abd.rc.left = 0;
-                abd.rc.right = SystemInformation.PrimaryMonitorSize.Width;
-                if (abd.uEdge == (int)ABEdge.ABE_TOP)
-                {
-                    abd.rc.top = 0;
-                    abd.rc.bottom = Size.Height;
-                }
-                else
-                {
-                    abd.rc.bottom = SystemInformation.PrimaryMonitorSize.Height;
-                    abd.rc.top = abd.rc.bottom - Size.Height;
-                }
-            }
-
-            // Query the system for an approved size and position.
-            SHAppBarMessage((int)ABMsg.ABM_QUERYPOS, ref abd);
-
-            // Adjust the rectangle, depending on the edge to which the
-            // appbar is anchored.
-            switch (abd.uEdge)
-            {
-                case (int)ABEdge.ABE_LEFT:
-                    abd.rc.right = abd.rc.left + Size.Width;
-                    break;
-
-                case (int)ABEdge.ABE_RIGHT:
-                    abd.rc.left = abd.rc.right - Size.Width;
-                    break;
-
-                case (int)ABEdge.ABE_TOP:
-                    abd.rc.bottom = abd.rc.top + Size.Height;
-                    break;
-
-                case (int)ABEdge.ABE_BOTTOM:
-                    abd.rc.top = abd.rc.bottom - Size.Height;
-                    break;
-            }
-
-            // Pass the final bounding rectangle to the system.
-            SHAppBarMessage((int)ABMsg.ABM_SETPOS, ref abd);
-
-            // Move and size the appbar so that it conforms to the
-            // bounding rectangle passed to the system.
-            MoveWindow(abd.hWnd, abd.rc.left, abd.rc.top,
-                abd.rc.right - abd.rc.left, abd.rc.bottom - abd.rc.top, true);
-        }
-
-        protected override void WndProc(ref System.Windows.Forms.Message m)
-        {
-            if (m.Msg == uCallBack)
-            {
-                switch (m.WParam.ToInt32())
-                {
-                    case (int)ABNotify.ABN_POSCHANGED:
-                        ABSetPos();
-                        break;
-                }
-            }
-
-            base.WndProc(ref m);
-        }
-
-        protected override System.Windows.Forms.CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                cp.Style &= (~0x00C00000); // WS_CAPTION
-                cp.Style &= (~0x00800000); // WS_BORDER
-                cp.ExStyle = 0x00000080 | 0x00000008; // WS_EX_TOOLWINDOW | WS_EX_TOPMOST
-                return cp;
-            }
-        }
-
-        private const int ABM_GETTASKBARPOS = 5;
-
-
-        private static Rectangle GetTaskbarPosition()
-        {
-            APPBARDATA data = new APPBARDATA();
-            data.cbSize = Marshal.SizeOf(data);
-
-            IntPtr retval = (IntPtr)SHAppBarMessage(ABM_GETTASKBARPOS, ref data);
-            if (retval == IntPtr.Zero)
-            {
-                throw new Win32Exception("Please re-install Windows");
-            }
-
-            return new Rectangle(data.rc.left, data.rc.top, data.rc.right - data.rc.left, data.rc.bottom - data.rc.top);
-        }
-
-        private static Color GetColourAt(Point location)
-        {
-            using (Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb))
-            using (Graphics gdest = Graphics.FromImage(screenPixel))
-            {
-                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
-                {
-                    IntPtr hSrcDC = gsrc.GetHdc();
-                    IntPtr hDC = gdest.GetHdc();
-                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
-                    gdest.ReleaseHdc();
-                    gsrc.ReleaseHdc();
-                }
-
-                return screenPixel.GetPixel(0, 0);
-            }
-        }
-
-        public static Color GetTaskbarColor()
-        {
-            return GetColourAt(GetTaskbarPosition().Location);
-        }
-        #endregion
-
+        private Panel panelMain;
+        private ContextMenuStrip contextMenuMain;
+        private ToolStripMenuItem exitToolStripMenuItem;
     }
 }
