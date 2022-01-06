b8d7ad230d7e6b3c391ba3284e58ea3dfa5a1e3c
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-06 10:00:53 +0400

    feat: adds RAM usage

---
+++ src/WinAppBar.Plugins.SystemResources/CpuUsage.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using Forms = System.Windows.Forms;
+
+namespace WinAppBar.Plugins.SystemResources
+{
+    internal class CpuUsage:Panel
+    {
+        readonly Label _label;
+        readonly PictureBox _pictureBox;
+        readonly Forms.Timer _timer;
+        readonly PerformanceCounter _cpuCounter;
+        readonly ColorTheme _colorTheme;
+
+        public CpuUsage():base()
+        {
+            _colorTheme = new ColorTheme();
+
+            this.Width = 75;
+            this.Padding = new Padding(5,0,5,0);
+
+            _label = new Label()
+            {
+                Dock = DockStyle.Right,
+                ForeColor = _colorTheme.TextColor,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Padding = new Padding(0, 0, 0, 0),
+                Text = $"---.--",
+                Width = 55
+            };
+
+            this.Controls.Add(_label);
+
+            _pictureBox = new PictureBox()
+            {
+                Width = 16,
+                Image = Bitmap.FromStream(this.GetType().Assembly.GetManifestResourceStream($"{this.GetType().Namespace}.cpu.png")),
+                SizeMode = PictureBoxSizeMode.CenterImage,
+                Dock = DockStyle.Left
+            };
+
+            this.Controls.Add(_pictureBox);
+            _pictureBox.BringToFront();
+
+            _cpuCounter = new PerformanceCounter()
+            {
+                CategoryName = "Processor",
+                CounterName = "% Processor Time",
+                InstanceName = "_Total",
+                ReadOnly = true
+            };
+
+            _timer = new Forms.Timer()
+            {
+                Interval = 1000
+            };
+
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+
+
+            Timer_Tick(_timer, new EventArgs());
+        }
+
+        private async void Timer_Tick(object? sender, EventArgs e)
+        {
+            var cpu = await Task.Run<double>(() =>
+            {
+                var total = _cpuCounter.NextValue(); // first call will always return 0
+                Thread.Sleep(750); // wait then try again
+                total = _cpuCounter.NextValue();
+                return Math.Round(total, 2);
+            });
+            this._label.Text = $"{cpu}%";
+        }
+    }
+}
--- src/WinAppBar.Plugins.SystemResources/Plugin.cs
+++ src/WinAppBar.Plugins.SystemResources/Plugin.cs
@@ -7,66 +7,32 @@ namespace WinAppBar.Plugins.SystemResources
     public class Plugin : PluginBase
     {
         public override event EventHandler ApplicationExit;
-
-        readonly Label _label;
-        readonly PictureBox _pictureBox;
-        readonly Forms.Timer _timer;
-        readonly PerformanceCounter _cpuCounter;
         readonly ContextMenuStrip _contextMenuStripMain;
         readonly ColorTheme _colorTheme;
+        readonly CpuUsage _cpuUsage;
+        readonly RamUsage _ramUsage;
 
         public Plugin() : base()
         {
             _colorTheme = new ColorTheme();
 
-            this.Width = 70;
+            _cpuUsage = new CpuUsage()
+            {
+                Dock = DockStyle.Right
+            };
+
+            _ramUsage = new RamUsage()
+            {
+                Dock = DockStyle.Right
+            };
+
+            this.Width = _cpuUsage.Width + _ramUsage.Width;
             this.Dock = DockStyle.Right;
 
-            _label = new Label()
-            {
-                Dock = DockStyle.Right,
-                ForeColor = _colorTheme.TextColor,
-                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
-                Padding = new Padding(0, 0, 5, 0),
-                Text = $"-.-%",
-                Width = 55
-            };
+            this.Controls.Add(_cpuUsage);
+            this.Controls.Add(_ramUsage);
 
-            this.Controls.Add(_label);
-
-            _pictureBox = new PictureBox()
-            {
-                Width = 16,
-                Image = Bitmap.FromStream(this.GetType().Assembly.GetManifestResourceStream($"{this.GetType().Namespace}.cpu.png")),
-                SizeMode = PictureBoxSizeMode.CenterImage,
-                Dock = DockStyle.Left
-            };
-
-            this.Controls.Add(_pictureBox);
-            _pictureBox.BringToFront();
-
-            //label.MouseEnter += Label_MouseEnter;
-            //label.MouseLeave += Label_MouseLeave;
-            //label.Click += Label_Click;
-
-            _cpuCounter = new PerformanceCounter()
-            {
-                CategoryName = "Processor",
-                CounterName = "% Processor Time",
-                InstanceName = "_Total",
-                ReadOnly = true
-            };
-
-            _timer = new Forms.Timer()
-            {
-                Interval = 1000
-            };
-
-            _timer.Tick += Timer_Tick;
-            _timer.Start();
-        
-
-            Timer_Tick(_timer, new EventArgs());
+            //this.BackColor = Color.Black;
 
             _contextMenuStripMain = new ContextMenuStrip()
             {
@@ -108,40 +74,9 @@ namespace WinAppBar.Plugins.SystemResources
 
         }
 
-        private async void Timer_Tick(object? sender, EventArgs e)
-        {
-            var cpu = await Task.Run<double>(() =>
-            {
-                var total = _cpuCounter.NextValue(); // first call will always return 0
-                Thread.Sleep(750); // wait then try again
-                total = _cpuCounter.NextValue();
-                return Math.Round(total, 2);
-            });
-            this._label.Text = $"{cpu}%";
-        }
-
-        private void Label_Click(object? sender, EventArgs e)
-        {
-            MouseEventArgs args = (MouseEventArgs)e;
-            if (args != null && args.Button == MouseButtons.Right)
-                return;
 
 
-        }
-
-        private void Label_MouseLeave(object? sender, EventArgs e)
-        {
-            var control = sender as Label;
-            if (control != null)
-                control.BackColor = Color.Transparent;
-        }
-
-        private void Label_MouseEnter(object? sender, EventArgs e)
-        {
-            var control = sender as Label;
-            if (control != null)
-                control.BackColor = _colorTheme.HoverColor;
-        }
+       
 
         public override async Task SaveConfig()
         {
