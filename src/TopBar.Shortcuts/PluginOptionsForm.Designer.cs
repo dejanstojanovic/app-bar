7745b69e681e8620e930ff0b2001bb341004c0b9
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-09 21:46:02 +0400

    feat: adds up/down icons

--- src/TopBar.Shortcuts/PluginOptionsForm.Designer.cs
+++ src/TopBar.Shortcuts/PluginOptionsForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
+            this.listviewShorcuts = new System.Windows.Forms.ListView();
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonCancel
@@ -52,6 +55,33 @@
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
+            // listviewShorcuts
+            // 
+            this.listviewShorcuts.Location = new System.Drawing.Point(57, 12);
+            this.listviewShorcuts.Name = "listviewShorcuts";
+            this.listviewShorcuts.Size = new System.Drawing.Size(360, 403);
+            this.listviewShorcuts.TabIndex = 4;
+            this.listviewShorcuts.UseCompatibleStateImageBehavior = false;
+            this.listviewShorcuts.View = System.Windows.Forms.View.Details;
+            // 
+            // buttonMoveUp
+            // 
+            this.buttonMoveUp.Image = global::TopBar.Plugins.Shortcuts.Properties.Resources.arrow_up_32;
+            this.buttonMoveUp.Location = new System.Drawing.Point(12, 164);
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.Size = new System.Drawing.Size(39, 35);
+            this.buttonMoveUp.TabIndex = 5;
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonMoveDown
+            // 
+            this.buttonMoveDown.Image = global::TopBar.Plugins.Shortcuts.Properties.Resources.arrow_down_32;
+            this.buttonMoveDown.Location = new System.Drawing.Point(12, 205);
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.Size = new System.Drawing.Size(39, 35);
+            this.buttonMoveDown.TabIndex = 6;
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            // 
             // PluginOptionsForm
             // 
             this.AcceptButton = this.buttonSave;
@@ -59,6 +89,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
             this.ClientSize = new System.Drawing.Size(429, 451);
+            this.Controls.Add(this.buttonMoveDown);
+            this.Controls.Add(this.buttonMoveUp);
+            this.Controls.Add(this.listviewShorcuts);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.buttonCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -76,5 +109,8 @@
         #endregion
         private Button buttonCancel;
         private Button buttonSave;
+        private ListView listviewShorcuts;
+        private Button buttonMoveUp;
+        private Button buttonMoveDown;
     }
 }
