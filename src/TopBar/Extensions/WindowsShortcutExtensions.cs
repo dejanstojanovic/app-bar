948d1b4df2cb2ebb359ee1ff318a170f0d0366a0
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-11 00:04:39 +0400

    feat: adds windows startup

---
+++ src/TopBar/Extensions/WindowsShortcutExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopBar.Extensions
+{
+    internal static class WindowsShortcutExtensions
+    {
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        internal class ShellLink
+        {
+        }
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        internal interface IShellLink
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+            void Resolve(IntPtr hwnd, int fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+        }
+
+        static void CreateShortcut(
+            string shortcutTarget, 
+            string workingFolder, 
+            string description,
+            string shortcutFilePath,
+            string shortcutFilename)
+        {
+            IShellLink link = (IShellLink)new ShellLink();
+
+            link.SetDescription(description);
+            link.SetPath(shortcutTarget);
+            link.SetWorkingDirectory(workingFolder);
+
+            //link.SetPath(Application.ExecutablePath);
+            //link.SetWorkingDirectory(Path.GetDirectoryName(Application.ExecutablePath));
+
+            IPersistFile file = (IPersistFile)link;
+            //string StartupFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            file.Save(Path.Combine(shortcutFilePath, !shortcutFilename.EndsWith(".lnk", StringComparison.InvariantCultureIgnoreCase) ? $"{shortcutFilename}.lnk" : shortcutFilename), false);
+        }
+    }
+}
