5de332586bd89f2e48188943a89af60fd125448c
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-11 00:06:52 +0400

    Feature/startup (#10)
    * feat: windows startup registry read/write
    
    * feat: dispose registry key object
    
    * feat: icon update
    
    * feat: improves startup check
    
    * feat: adds windows startup

--- src/TopBar/Configuration.cs
+++ src/TopBar/Configuration.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.Text.Json.Serialization;
 
 namespace TopBar
 {
     public class Configuration
     {
+        const string StartupRunRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+
         public string ScreenDeviceName { get; set; }
+
+        [JsonIgnore]
+        public bool RunOnStartup
+        {
+            get
+            {
+                using (var startupRunKey = Registry.CurrentUser.OpenSubKey(StartupRunRegistryKey, false))
+                {
+                    if (startupRunKey != null)
+                    {
+                        var value = startupRunKey.GetValue(this.GetType().Namespace);
+                        return value != null && value.ToString().Equals($"\"{Application.ExecutablePath}\"", StringComparison.InvariantCultureIgnoreCase);
+                    }
+                    return false;
+                }
+            }
+            set
+            {
+                using (var startupRunKey = Registry.CurrentUser.OpenSubKey(StartupRunRegistryKey, true))
+                {
+                    if (value)
+                        startupRunKey.SetValue(this.GetType().Namespace, $"\"{Application.ExecutablePath}\"");
+                    else
+                        startupRunKey.DeleteValue(this.GetType().Namespace);
+                }
+            }
+        }
     }
 }
