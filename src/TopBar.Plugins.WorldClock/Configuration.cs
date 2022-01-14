f1f3f12e3ec28dd650964f5bedfcc2f4485a97a8
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-14 23:48:15 +0400

    feat: adds WorldClock plugin project

---
+++ src/TopBar.Plugins.WorldClock/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopBar.Plugins.WorldClock
+{
+    public class Configuration
+    {
+        IEnumerable<ClockConfiguration> Clocks { get;}
+    }
+
+    public class ClockConfiguration
+    {
+        public string Title { get; set; }
+        public string TimeZoneId { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ src/TopBar.Plugins.WorldClock/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopBar.Plugins.WorldClock
+{
+    public class Plugin : PluginBase
+    {
+        public override string Name => "World Clock";
+
+        public override IEnumerable<ToolStripMenuItem> MenuItems => null;
+
+        public override async Task SaveConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
