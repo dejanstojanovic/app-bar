ae073ba0561a46f6fb1ee6fbcb5eaa2707065ec2
Dejan Stojanovic <d.stojanovic@hotmail.com>
2022-01-16 15:56:34 +0400

    feat: dropdown edit (initial)

--- src/TopBar.Plugins.WorldClock/PluginOptionsForm.Designer.cs
+++ src/TopBar.Plugins.WorldClock/PluginOptionsForm.Designer.cs
@@ -28,17 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PluginOptionsForm));
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
             this._listviewShorcuts = new System.Windows.Forms.ListView();
-            this.headerText = new System.Windows.Forms.ColumnHeader();
+            this.headerActive = new System.Windows.Forms.ColumnHeader();
+            this.headerLabel = new System.Windows.Forms.ColumnHeader();
+            this.headerTimeZone = new System.Windows.Forms.ColumnHeader();
             this.buttonMoveUp = new System.Windows.Forms.Button();
             this.buttonMoveDown = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(342, 421);
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(620, 421);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 2;
@@ -48,7 +52,8 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(261, 421);
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(539, 421);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 3;
@@ -58,26 +63,42 @@
             // 
             // _listviewShorcuts
             // 
+            this._listviewShorcuts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this._listviewShorcuts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.headerText});
+            this.headerActive,
+            this.headerLabel,
+            this.headerTimeZone});
             this._listviewShorcuts.FullRowSelect = true;
-            this._listviewShorcuts.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this._listviewShorcuts.LabelEdit = true;
+            this._listviewShorcuts.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this._listviewShorcuts.Location = new System.Drawing.Point(57, 12);
             this._listviewShorcuts.MultiSelect = false;
             this._listviewShorcuts.Name = "_listviewShorcuts";
-            this._listviewShorcuts.Size = new System.Drawing.Size(360, 403);
+            this._listviewShorcuts.Size = new System.Drawing.Size(638, 403);
             this._listviewShorcuts.TabIndex = 4;
             this._listviewShorcuts.UseCompatibleStateImageBehavior = false;
             this._listviewShorcuts.View = System.Windows.Forms.View.Details;
+            this._listviewShorcuts.SelectedIndexChanged += new System.EventHandler(this._listviewShorcuts_SelectedIndexChanged);
             // 
-            // headerText
+            // headerActive
             // 
-            this.headerText.Text = "";
-            this.headerText.Width = 350;
+            this.headerActive.Text = "Active";
+            this.headerActive.Width = 50;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.Text = "Label";
+            this.headerLabel.Width = 250;
+            // 
+            // headerTimeZone
+            // 
+            this.headerTimeZone.Text = "Time Zone";
+            this.headerTimeZone.Width = 300;
             // 
             // buttonMoveUp
             // 
+            this.buttonMoveUp.Image = ((System.Drawing.Image)(resources.GetObject("buttonMoveUp.Image")));
             this.buttonMoveUp.Location = new System.Drawing.Point(12, 164);
             this.buttonMoveUp.Name = "buttonMoveUp";
             this.buttonMoveUp.Size = new System.Drawing.Size(39, 35);
@@ -86,6 +107,7 @@
             // 
             // buttonMoveDown
             // 
+            this.buttonMoveDown.Image = ((System.Drawing.Image)(resources.GetObject("buttonMoveDown.Image")));
             this.buttonMoveDown.Location = new System.Drawing.Point(12, 205);
             this.buttonMoveDown.Name = "buttonMoveDown";
             this.buttonMoveDown.Size = new System.Drawing.Size(39, 35);
@@ -98,7 +120,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(429, 451);
+            this.ClientSize = new System.Drawing.Size(707, 451);
             this.Controls.Add(this.buttonMoveDown);
             this.Controls.Add(this.buttonMoveUp);
             this.Controls.Add(this._listviewShorcuts);
@@ -120,8 +142,10 @@
         private Button buttonMoveDown;
         private Button buttonMoveUp;
         private ListView _listviewShorcuts;
-        private ColumnHeader headerText;
+        private ColumnHeader headerLabel;
         private Button buttonSave;
         private Button buttonCancel;
+        private ColumnHeader headerActive;
+        private ColumnHeader headerTimeZone;
     }
 }
--- src/TopBar.Plugins.WorldClock/PluginOptionsForm.cs
+++ src/TopBar.Plugins.WorldClock/PluginOptionsForm.cs
@@ -7,19 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.ListViewItem;
 
 namespace TopBar.Plugins.WorldClock
 {
     public partial class PluginOptionsForm : Form
     {
+        readonly ComboBox _timeZonesList;
         public PluginOptionsForm()
         {
+            _timeZonesList = new ComboBox() { Visible = false };
+            _timeZonesList.Items.Add("Option 1");
+            _timeZonesList.Items.Add("Option 2");
+            _timeZonesList.Items.Add("Option 3");
+
+            this.Controls.Add(_timeZonesList);
+
             InitializeComponent();
+            this._listviewShorcuts.MouseDoubleClick += _listviewShorcuts_MouseDoubleClick;
         }
+
+        private void _listviewShorcuts_MouseDoubleClick(object? sender, MouseEventArgs e)
+        {
+            //https://www.codeproject.com/Articles/316204/ListView-in-line-editing
+
+
+            var currentItem = _listviewShorcuts.GetItemAt(e.X, e.Y);
+            if (currentItem == null)
+                return;
+
+            var currentSubItem = currentItem.GetSubItemAt(e.X, e.Y);
+            if (currentSubItem == null)
+                return;
+
+            var lLeft = currentSubItem.Bounds.Left + 2;
+            var lWidth = currentSubItem.Bounds.Width;
+
+            _timeZonesList.SetBounds(lLeft + _listviewShorcuts.Left, currentSubItem.Bounds.Top + _listviewShorcuts.Top, lWidth, currentSubItem.Bounds.Height);
+            _timeZonesList.Show();
+            _timeZonesList.Focus();
+
+
+        }
+
+
 
         private void PluginOptionsForm_Load(object sender, EventArgs e)
         {
+            var item = new ListViewItem();
 
+            var subitems = new ListViewSubItem[] {
+            new ListViewSubItem(item,"Text1"),
+            new ListViewSubItem(item,"Text2"),
+            new ListViewSubItem(item,"Text3")
+            };
+
+            item.SubItems.AddRange(subitems);
+
+            this._listviewShorcuts.Items.Add(item);
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
@@ -32,6 +77,11 @@ namespace TopBar.Plugins.WorldClock
         {
             this.DialogResult = DialogResult.OK;
             Close();
+        }
+
+        private void _listviewShorcuts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
